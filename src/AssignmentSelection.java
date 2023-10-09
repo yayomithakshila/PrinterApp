d056fe9a4db150f6ee28645e69636caa080741f6
Yayomi Thakshila Premathilaka <yayomifin@outlook.com>
2023-10-09 14:20:39 +0300

    Initial commit

---
+++ src/AssignmentSelection.java
@@ -0,0 +1,45 @@
+
+public class AssignmentSelection {
+
+	public static void main(String[] args) {
+		
+		int number = 120 ;
+		 
+		   if (number > 100)
+		   System.out.println(number);
+
+		
+		
+//		String os1,os2 ;
+//		os1 = "iOS" ;
+//		os2 = "Android";
+//		System.out.println(os1);
+//		System.out.println(os2);
+//		
+//		
+//	    if (os1 == os2)
+//	    {System.out.println("Strings are equal");}
+//	
+//	    else if (os1 != os2)
+//	    {System.out.println("Strings are not equal");}
+//	
+		
+//      int number1,number2,number3 ;
+//      number1 = 10 ;
+//      number2 = 20 ;
+//      number3 = 10 ;
+//      
+//      if (number1==number2 && number2==number3)
+//      {System.out.println("Numbers are equal");}
+//      
+//      else 
+//      {System.out.println("Numbers are not equal");}
+//      
+//      
+		
+		
+		
+		
+	}
+
+}
---
+++ src/Printer.java
@@ -0,0 +1,57 @@
+
+public class Printer {
+
+	public static void main(String[] args) {
+		System.out.println("Printer App");
+		
+		String creater;
+		creater = "Yayomi" ;
+		System.out.println("This app is made by " + creater);
+		
+		int Number1 = 10;
+		int Number2 = 20;
+		System.out.println(Number1);
+	System.out.println(Number2);
+
+// Subtraction
+	int number1 = 20;
+		int number2 = 10;
+		System.out.println("The value of number1 is " + number1);		System.out.println("The value of number2 is " + number2);
+
+		int multiplication;
+		multiplication = number1 * number2 ;
+		System.out.println (multiplication) ;
+		
+//// Addition		
+		
+		
+		int Addition ;
+		Addition = number1 + number2 ; 
+		System.out.println (Addition) ;
+		
+//// Subtraction
+		
+		
+		
+		int Subtraction ;
+		Subtraction = number1 - number2 ;
+		System.out.println (Subtraction) ;
+		
+//// Division
+	
+		
+		double division ;
+		division = number1 / number2 ;
+		System.out.println (division) ;		
+		
+	
+		
+
+		
+		
+		
+   
+		
+	}
+
+}
